cd50b07165e10b27baac40ce1c07b963f56dea2a
gamerforEA <gamerforEA@gmail.com>
2015-03-22 19:17:01 +0300

    Init commit.

---
+++ src/main/java/appeng/items/tools/quartz/ToolQuartzWrench.java
@@ -0,0 +1,78 @@
+package appeng.items.tools.quartz;
+
+import java.util.EnumSet;
+
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
+import appeng.api.implementations.items.IAEWrench;
+import appeng.api.util.DimensionalCoord;
+import appeng.core.features.AEFeature;
+import appeng.items.AEBaseItem;
+import appeng.transformer.annotations.integration.Interface;
+import appeng.util.Platform;
+import buildcraft.api.tools.IToolWrench;
+
+@Interface(iface = "buildcraft.api.tools.IToolWrench", iname = "BC")
+public class ToolQuartzWrench extends AEBaseItem implements IAEWrench, IToolWrench
+{
+	public ToolQuartzWrench(AEFeature type)
+	{
+		super(ToolQuartzWrench.class, type.name());
+		setfeature(EnumSet.of(type, AEFeature.QuartzWrench));
+		setMaxStackSize(1);
+	}
+
+	@Override
+	public boolean onItemUseFirst(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
+	{
+		Block b = world.getBlock(x, y, z);
+		if (b != null && !player.isSneaking() && Platform.hasPermissions(new DimensionalCoord(world, x, y, z), player))
+		{
+			if (Platform.isClient()) return !world.isRemote;
+
+			// TODO gamerforEA code start
+			BreakEvent event = new BreakEvent(x, y, z, world, b, world.getBlockMetadata(x, y, z), player);
+			if (MinecraftForge.EVENT_BUS.post(event)) return false;
+			// TODO gamerforEA code end
+
+			ForgeDirection mySide = ForgeDirection.getOrientation(side);
+			if (b.rotateBlock(world, x, y, z, mySide))
+			{
+				b.onNeighborBlockChange(world, x, y, z, Platform.air);
+				player.swingItem();
+				return !world.isRemote;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	// public boolean shouldPassSneakingClickToBlock(World w, int x, int y, int z)
+	public boolean doesSneakBypassUse(World world, int x, int y, int z, EntityPlayer player)
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canWrench(ItemStack is, EntityPlayer player, int x, int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public boolean canWrench(EntityPlayer player, int x, int y, int z)
+	{
+		return true;
+	}
+
+	@Override
+	public void wrenchUsed(EntityPlayer player, int x, int y, int z)
+	{
+		player.swingItem();
+	}
+}
